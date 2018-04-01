966188d4b102777d87cf3f2f8e386be222b72829
dorogov <36375571+dorogov@users.noreply.github.com>
2018-04-01 13:06:59 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 
 def load_data(filepath):
@@ -6,22 +7,21 @@ def load_data(filepath):
         return json.load(file)
 
 
-def make_pretty_json(json_data):
-    return json.dumbs(json_data, ensure_ascii=False, indent=4, sort_keys=True)
+def make_pretty_json(json_content):
+    return json.dumps(json_content, ensure_ascii=False, indent=4, sort_keys=True)
 
 
-def parser():
+def parsed_file_name():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-f', 
-        '--file', 
-        required=True, 
+        '-f',
+        '--file',
+        required=True,
         metavar='ФАЙЛ',
         help='Путь до файла .json')
     return parser.parse_args()
-   
-    
+
+
 if __name__ == '__main__':
-    args = parser()
-    dict_data = load_data(args.file)
-    print(make_pretty_json(json_data))
+    dict_data = load_data(parsed_file_name().file)
+    print(make_pretty_json(dict_data))
